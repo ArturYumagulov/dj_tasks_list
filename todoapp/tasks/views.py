db6fde0d324539be880c120ead9e7f485313edec
YumagulovA <zico.13288@gmail.com>
2020-09-29 16:55:40 +0300

    Users acounts

--- todoapp/tasks/views.py
+++ todoapp/tasks/views.py
@@ -6,15 +6,24 @@ from django.views import View
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
+
+@login_required
 def index(requests):
     return HttpResponse("Hello guest")
 
 
-class TaskListView(ListView):
-    queryset = ToDoItem.objects.all()
+class TaskListView(LoginRequiredMixin, ListView):
+    model = ToDoItem
+    # queryset = ToDoItem.objects.all()
     context_object_name = "tasks"
     template_name = 'tasks/lists.html'
+
+    def get_queryset(self):
+        u = self.request.user
+        return u.tasks.all()
 
 
 def complete_task(requests, uid):
@@ -34,8 +43,13 @@ class TaskCreateView(View):
     def post(self, request, *args, **kwargs):
         form = ToDoItemFormModel(request.POST)
         if form.is_valid():
-            form.save()
+            print("request.user == ", request.user)
+            new_task = form.save(commit=False)
+            print("new_task_owner: ", new_task)
+            new_task.owner = request.user
+            new_task.save()
             return redirect('/tasks/lists/')
+
         return render(request, 'tasks/create.html', {'form': form})
 
     def get(self, request, *args, **kwargs):
